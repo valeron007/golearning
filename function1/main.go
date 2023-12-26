874591387fe8ac0e6ff7f3e8f9348ce23a770452
valeron <valeronchik0702@gmail.com>
2023-12-26 08:20:08 +0300

    string array

--- function1/main.go
+++ function1/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(^uint(0) >> 1)
+	s := "world"
+	//buffer := bytes.Buffer{}
+	length := len(s)
+	var arr = make([]string, length)
+	for i, c := range s {
+		//fmt.Printf("%c", c)
+		arr[i] = string(c)
+		println(arr[i])
+	}
+	fmt.Println()
+	fmt.Println(arr)
+	res := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		res = res + string(s[i])
+		/*
+			fmt.Println(res)
+			fmt.Println(strconv.FormatUint(uint64(s[i]), 10))
+			buffer.WriteString(strconv.FormatUint(uint64(s[i]), 10))
+		*/
+	}
+	//fmt.Println(s[len(s):])
+	//t := string(buf)
+	//var result string = buffer.String()
+	//fmt.Println(t)
+
 }
